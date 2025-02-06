cec4388c719fcb1bef7c31d34392a75157bca53e
Hassan Raza Malik <hassan.raza@sysreformsint.com>
2025-02-06 09:26:20 +0500

    new fixes according to Hero Section

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,14 @@ export default function Home() {
    <main className="relative bg-black-100 flex justify-center items-center overflow-hidden flex-col mx-auto sm:px-10 px-5">
    <div className="max-w-7xl w-full">
     {/* <h1>Hello, Portfolio</h1> */}
+    <FloatingNav 
+    navItems={[
+      {name: "Home", link: "/"},
+      {name: "About", link: "/about"},
+      {name: "Projects", link: "/projects"},
+      {name: "Contact", link: "/contact"},
+    ]}
+    />
     <Hero />
    </div>
    </main>
